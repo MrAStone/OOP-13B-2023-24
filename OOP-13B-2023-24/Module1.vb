876912923c6092ed0ce32caab8de8755e8948ed8
A Stone <astone@heckgrammar.co.uk>
2023-09-19 09:59:31 +0100

    Add project files.

---
+++ OOP-13B-2023-24/Module1.vb
@@ -0,0 +1,24 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim b As New BankAccount("Mr Stone", 0.32)
+        Console.WriteLine(b.getBalance)
+    End Sub
+
+End Module
+Class BankAccount
+    Private AcNum As String
+    Private ACBalance As Decimal
+    Private ACHolder As String
+    Public Sub New(_ACHolder As String, OpenBalance As Decimal)
+        ACHolder = _ACHolder
+        ACBalance = OpenBalance
+    End Sub
+    Public Sub Deposit(amount As Decimal)
+        ACBalance += amount
+    End Sub
+    Public Function getBalance()
+        Return ACBalance
+    End Function
+End Class
+
